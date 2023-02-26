4a111cd216a9b4dd28e1c06e0fd670fe6881f5f6
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2023-02-27 04:02:24 +0530

    Update Solution.java

--- leetcode/581. Shortest Unsorted Continuous Subarray/Solution.java
+++ leetcode/581. Shortest Unsorted Continuous Subarray/Solution.java
@@ -35,3 +35,47 @@ class Solution {
     }
 }
 
+// Using stack
+// t.c.=O(n) s.c.=O(n)
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int n=nums.length;
+        Stack<Integer> stack = new Stack<Integer>();
+        int start=n, end=0;
+        for(int i=0;i<n;i++) {
+            while(!stack.isEmpty() && nums[stack.peek()]>nums[i])
+                start = Math.min(start, stack.pop());
+            stack.push(i);
+        }
+        stack.clear();
+        for(int i=n-1;i>=0;i--) {
+            while(!stack.isEmpty() && nums[stack.peek()]<nums[i])
+                end=Math.max(end, stack.pop());
+            stack.push(i);
+        }
+        return end-start<0? 0:end-start+1;
+    }
+}
+
+// Without stack but same concepts
+// t.c.=O(n), s.c.=O(1)
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int n=nums.length;
+        int min=Integer.MAX_VALUE, max=Integer.MIN_VALUE;
+        int start, end;
+        for(int i=1;i<n;i++) 
+            if(nums[i]<nums[i-1])
+                min=Math.min(min, nums[i]);
+        for(int i=n-2;i>=0;i--)
+            if(nums[i]>nums[i+1])
+                max=Math.max(max, nums[i]);
+        for(start=0;start<n;start++) 
+            if(min<nums[start])
+                break;
+        for(end=n-1;end>=0;end--)
+            if(max>nums[end])
+                break;
+        return end-start<0? 0:end-start+1;
+    }
+}
