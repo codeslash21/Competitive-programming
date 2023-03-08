15d3b4e9e93d7879f073aa38942e09c052b5f11c
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2023-03-08 12:26:25 +0530

    Update Solution.java

--- leetcode/137. Single Number II/Solution.java
+++ leetcode/137. Single Number II/Solution.java
@@ -40,3 +40,16 @@ class Solution {
         return (int)((3*setSum-arraySum)/2);
     }
 }
+
+// Using bitwise xor, not, and
+// t.c.=O(n), s.c.=O(1)
+class Solution {
+    public int singleNumber(int[] nums) {
+        int seenOnce=0, seenTwice=0;
+        for(int num:nums) {
+            seenOnce=~seenTwice & (seenOnce^num);
+            seenTwice=~seenOnce & (seenTwice ^ num);
+        }
+        return seenOnce;
+    }
+}
