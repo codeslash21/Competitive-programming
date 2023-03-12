0d154d01243ae80aafdb5ad331e4d849d43d1e16
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2023-03-12 10:22:22 +0530

    Create Solution.java

---
+++ leetcode/252. Meeting Rooms/Solution.java
@@ -0,0 +1,12 @@
+// using sorting
+// t.c.=O9nlogn), s.c.=O(1)
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+        int n=intervals.length;
+        Arrays.sort(intervals, (a,b)->Integer.compare(a[0], b[0]));
+        for(int i=0; i<n-1; i++) 
+            if(intervals[i][1]>intervals[i+1][0])
+                return false;
+        return true;
+    }
+}
