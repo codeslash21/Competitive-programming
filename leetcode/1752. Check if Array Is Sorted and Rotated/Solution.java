fc2d02ae98d03fdc4f783037047c8827191a05f3
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2023-03-22 03:07:41 +0530

    Create Solution.java

---
+++ leetcode/1752. Check if Array Is Sorted and Rotated/Solution.java
@@ -0,0 +1,12 @@
+// linear search
+// t.c.=O(n), s.c.=O(1)
+class Solution {
+    public boolean check(int[] nums) {
+        int n=nums.length;
+        int cnt=0;
+        for(int i=0;i<n;i++) 
+            if(nums[i]>nums[(i+1)%n])
+                cnt++;
+        return cnt>1? false:true;
+    }
+}
