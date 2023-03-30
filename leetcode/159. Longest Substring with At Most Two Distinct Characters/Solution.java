7c645c4cc933bd9f4504c45cda948601d0d9e560
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2023-03-30 15:26:40 +0530

    Create Solution.java

---
+++ leetcode/159. Longest Substring with At Most Two Distinct Characters/Solution.java
@@ -0,0 +1,20 @@
+// Using sliding window
+// t.c.=O(n), s.c.=O(n)
+class Solution {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int l=0, r=0;
+        int max=0;
+        int required=2;
+        while(r<s.length()) {
+            map.put(s.charAt(r), r++);
+            if(map.size()>required) {
+                int idx_val=Collections.min(map.values());
+                map.remove(s.charAt(idx_val));
+                l=idx_val+1;
+            }
+            max=Math.max(max, r-l);
+        }
+        return max;
+    }
+}
