6a8c95ad7dbc46b0451fb08f31ea9236b0fe8d6c
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2023-04-15 03:56:31 +0530

    Create Solution.java

---
+++ leetcode/328. Odd Even Linked List/Solution.java
@@ -0,0 +1,16 @@
+// t.c.=O(n), s.c.=O(1)
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head==null)
+            return null;
+        ListNode odd=head, even=head.next, evenHead=even;
+        while(even!=null && even.next!=null) {
+            odd.next=even.next;
+            odd=odd.next;
+            even.next=odd.next;
+            even=even.next;
+        }
+        odd.next=evenHead;
+        return head;
+    }
+}
