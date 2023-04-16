c3a10113f3428c2f9b2ad44fe9480d9ad8e526b8
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2023-04-16 15:29:23 +0530

    Create Solution.java

---
+++ leetcode/708. Insert into a Sorted Circular Linked List/Solution.java
@@ -0,0 +1,35 @@
+// Using two pointers
+// t.c.=O(n), s.c.=O(1)
+class Solution {
+    public Node insert(Node head, int insertVal) {
+        Node node = new Node(insertVal);
+        if(head==null) {
+            node.next=node;
+            return node;
+        }
+        if(head.next==null) {
+            head.next=node;
+            node.next=head;
+            return head;
+        }
+        Node prev=head, curr=head.next;
+        boolean toInsert=false;
+        do {
+            if(prev.val<=insertVal && insertVal<=curr.val)
+                toInsert=true;
+            else if(prev.val>curr.val)
+                if(prev.val<=insertVal || insertVal<=curr.val)
+                    toInsert=true;
+            if(toInsert) {
+                prev.next=node;
+                node.next=curr;
+                return head;
+            }
+            prev=curr;
+            curr=curr.next;
+        } while(prev!=head);
+        prev.next=node;
+        node.next=curr;
+        return head;
+    }
+}
