6695e84f14e302e0c5c2572738255a496fc501db
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2023-04-17 17:56:10 +0530

    Create Solution.java

---
+++ leetcode/277. Find the Celebrity/Solution.java
@@ -0,0 +1,15 @@
+// Linear solution
+// t.c.=O(n), s.c.=O(1)
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        int celeb=0;
+        for(int i=1;i<n;i++)
+            if(knows(celeb, i))
+                celeb=i;
+        for(int i=0;i<n;i++) {
+            if(i!=celeb && (!knows(i, celeb) || knows(celeb, i)))
+                return -1;
+        }
+        return celeb;
+    }
+}
