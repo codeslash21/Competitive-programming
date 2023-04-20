e95f98b4c9d26140f03a6ebc5f079b1de031b551
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2023-04-20 11:45:34 +0530

    Create Solution.java

---
+++ leetcode/100. Same Tree/Solution.java
@@ -0,0 +1,11 @@
+// Using recursion
+// t.c.=O(n), s.c.=O(n)
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null)
+            return true;
+        else if(p==null || q==null || (p.val!=q.val))
+            return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
