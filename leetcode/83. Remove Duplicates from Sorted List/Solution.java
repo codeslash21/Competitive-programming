bbf04bb413e1794cc3c2aa01e45e40d8aa747ecc
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2023-06-24 03:55:16 +0530

    Create Solution.java

---
+++ leetcode/83. Remove Duplicates from Sorted List/Solution.java
@@ -0,0 +1,12 @@
+// t.c.=O(n), s.c.=O(1)
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr=head;
+        while(curr!=null && curr.next!=null) 
+            if(curr.next.val==curr.val)
+                curr.next = curr.next.next;
+            else
+                curr=curr.next;
+        return head;
+    }
+}
