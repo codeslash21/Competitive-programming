b43ff1f1942d9196224e5b8a738745e799bc09d7
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2023-06-24 03:34:00 +0530

    Create Solution.java

---
+++ leetcode/203. Remove Linked List Elements/Solution.java
@@ -0,0 +1,14 @@
+// t.c.=O(n), s.c.=O(1)
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode curr = dummy;
+        while(curr.next!=null) {
+            if(curr.next.val==val) 
+                curr.next=curr.next.next;
+            else
+                curr=curr.next;
+        }
+        return dummy.next;
+    }
+}
