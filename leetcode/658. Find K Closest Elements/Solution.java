749e71a2163cb43f19ee7bbdacccc40284b5eaf8
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2023-10-21 19:04:11 +0530

    Update Solution.java

--- leetcode/658. Find K Closest Elements/Solution.java
+++ leetcode/658. Find K Closest Elements/Solution.java
@@ -11,3 +11,33 @@ class Solution {
         return sorted;
     }
 }
+
+// Using sliding window
+// t.c.=O(logn + k), s.c.=O(1)
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> res = new ArrayList<>();
+        int left=0, n=arr.length, right=n-1;
+        while(left<right) {
+            int mid=(left+right)/2;
+            if(arr[mid]>=x)
+                right=mid;
+            else
+                left=mid+1;
+        }
+        left-=1;
+        while(right-left<=k) {
+            if(left==-1) {
+                right++;
+                continue;
+            }
+            if(right==n || Math.abs(arr[left]-x)<=Math.abs(arr[right]-x))
+                left-=1;
+            else
+                right+=1;
+        }
+        for(int i=left+1; i<right; i++)
+            res.add(arr[i]);
+        return res;
+    }
+}
