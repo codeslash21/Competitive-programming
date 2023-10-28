79966a2d5f2484a2b174da39475cd4b1828ae887
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2023-10-28 15:54:21 +0530

    Update Solution.java

--- leetcode/1162. As Far from Land as Possible/Solution.java
+++ leetcode/1162. As Far from Land as Possible/Solution.java
@@ -30,3 +30,28 @@ class Solution {
         return distance<=0?-1:distance;
     }
 }
+
+// Using dp
+// t.c.=O(n^2), s.c.=O(n^2)
+class Solution {
+    public int maxDistance(int[][] grid) {
+        int n=grid.length;
+        int max_dist = n+n+1;
+        int[][] dist = new int[n][n];
+        for(int[] arr:dist)
+            Arrays.fill(arr, max_dist);
+        for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++) 
+                if(grid[i][j]==1)
+                    dist[i][j]=0;
+                else 
+                    dist[i][j]=Math.min(dist[i][j], Math.min(i>0?dist[i-1][j]+1:max_dist, j>0?dist[i][j-1]+1:max_dist));
+        int ans=0;
+        for(int i=n-1;i>=0;i--)
+            for(int j=n-1;j>=0;j--){
+                dist[i][j]=Math.min(dist[i][j], Math.min(i<n-1?dist[i+1][j]+1:max_dist, j<n-1?dist[i][j+1]+1:max_dist));
+                ans=Math.max(ans, dist[i][j]);
+            }
+        return ans==0||ans==max_dist?-1:ans;
+    }
+}
