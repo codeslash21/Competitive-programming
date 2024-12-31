4f7d1b4fd265b6c4e5b8bc677845c31cb344253b
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2024-12-31 11:27:42 +0530

    Update Main.java

--- codeforces/2053B/Main.java
+++ codeforces/2053B/Main.java
@@ -1,19 +1,22 @@
 //https://codeforces.com/contest/2053/problem/B
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine());
         while(t-->0) {
-            int n = sc.nextInt();
+            int n = Integer.parseInt((br.readLine()));
             int[] cnt = new int[2*n+1];
             int[] pref = new int[2*n+1];
             int[] l = new int[n];
             int[] r = new int[n];
             for(int i=0;i<n;i++) {
-                l[i] = sc.nextInt();
-                r[i] = sc.nextInt();
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                l[i] = Integer.parseInt(st.nextToken());
+                r[i] = Integer.parseInt(st.nextToken());
                 if(l[i]==r[i])
                     cnt[l[i]]++;
             }
@@ -25,17 +28,18 @@ public class Main {
             for(int i=0;i<n;i++) {
                 if(l[i]==r[i]) {
                     if(cnt[l[i]]<2)
-                        System.out.print(1);
+                        pw.print(1);
                     else
-                        System.out.print(0);
+                        pw.print(0);
                 } else {
                     if(pref[r[i]]-pref[l[i]-1]==r[i]-l[i]+1)
-                        System.out.print(0);
+                        pw.print(0);
                     else
-                        System.out.print(1);
+                        pw.print(1);
                 }
             }
-            System.out.println("");
+            pw.println("");
         }
+        pw.close();
     }
 }
