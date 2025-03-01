446c3850ab1dc64a3f7637434800d35a76cf42bd
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2025-03-01 23:50:05 +0530

    Create Main.java

---
+++ codeforces/CF630I/Main.java
@@ -0,0 +1,10 @@
+// https://codeforces.com/problemset/problem/630/I
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println(9*n - 3 << 2*n - 6);
+    }
+}
