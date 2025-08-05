631bf6fcdc4ef31e419ba2e57532a7acec3866dd
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2025-08-05 16:17:52 +0530

    Update Solution.java

--- leetcode/1235. Maximum Profit in Job Scheduling/Solution.java
+++ leetcode/1235. Maximum Profit in Job Scheduling/Solution.java
@@ -37,3 +37,50 @@ class Solution {
         return maxProfit;
     }
 }
+
+
+// Using DP and binary search
+// t.c.=O(nlogn), s.c.=O(n)
+class Solution {
+    int[] memo;
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        List<List<Integer>> jobs = new ArrayList<>();
+        int length = startTime.length;
+        int[] startTimes = new int[length];
+        memo = new int[length];
+        Arrays.fill(memo, -1);
+        for(int i=0;i<length;i++) {
+            List<Integer> currJob = new ArrayList<>();
+            currJob.add(startTime[i]);
+            currJob.add(endTime[i]);
+            currJob.add(profit[i]);
+            jobs.add(currJob);
+        }
+        Collections.sort(jobs, (a,b) -> a.get(0)-b.get(0));
+        for(int i=0;i<length;i++)
+            startTimes[i] = jobs.get(i).get(0);
+        return findMaxProfit(jobs, startTimes, length, 0);
+    }
+    public int findMaxProfit(List<List<Integer>> jobs, int[] startTimes, int length, int position) {
+        if(position==length)
+            return 0;
+        if(memo[position]!=-1)
+            return memo[position];
+        int nextIndex = findNextJob(startTimes, jobs.get(position).get(1), position);
+        int maxProfit = Math.max(findMaxProfit(jobs, startTimes, length, position+1), jobs.get(position).get(2)+findMaxProfit(jobs, startTimes, length, nextIndex));
+        memo[position]=maxProfit;
+        return memo[position];
+    }
+    public int findNextJob(int[] startTimes, int lastEndTime, int lastIndex) {
+        int left=lastIndex, right=startTimes.length-1, nextIndex=startTimes.length;
+        while(left<=right) {
+            int mid=left+(right-left)/2;
+            if(startTimes[mid]>=lastEndTime) {
+                nextIndex=mid;
+                right=mid-1;
+            } else
+                left=mid+1;
+        }
+        return nextIndex;
+    }
+}
