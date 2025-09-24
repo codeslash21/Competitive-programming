09dbba3b7e81fc74bfc2a7f3be359f0f6862099a
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2025-09-24 16:05:17 +0530

    Create Main.java

---
+++ codeforces/1794B/Main.java
@@ -0,0 +1,21 @@
+// https://codeforces.com/problemset/problem/1794/B
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0) {
+            int n=sc.nextInt();
+            int[] arr = new int[n];
+            for(int i=0;i<n;i++)
+                arr[i] = sc.nextInt()+1;
+            for(int i=1;i<n;i++)
+                if(arr[i]%arr[i-1]==0)
+                    arr[i]++;
+            for(int i=0;i<n;i++)
+                System.out.print(arr[i]+" ");
+            System.out.println();
+        }
+    }
+}
