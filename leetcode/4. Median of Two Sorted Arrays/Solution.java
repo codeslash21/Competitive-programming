fba2dd6cb5ea4c4ea7d4918b712fe388b616b420
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2025-09-28 23:27:58 +0530

    Update Solution.java

--- leetcode/4. Median of Two Sorted Arrays/Solution.java
+++ leetcode/4. Median of Two Sorted Arrays/Solution.java
@@ -26,24 +26,26 @@ class Solution {
 // t.c.=O(log(min(m,n))), s.c.=O(1)
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if(nums1.length>nums2.length)
-                return findMedianSortedArrays(nums2, nums1);
-        int m=nums1.length, n=nums2.length, left=0, right=m;
+        int len1=nums1.length, len2=nums2.length, totalLength=len1+len2, parity=totalLength%2;
+        if(len2>len1)
+            return findMedianSortedArrays(nums2, nums1);
+        int left=0, right=len2;
         while(left<=right) {
-            int partA=(left+right)/2, partB=(m+n+1)/2-partA;
-            int maxLeftA=partA==0?Integer.MIN_VALUE:nums1[partA-1];
-            int minRightA=partA==m?Integer.MAX_VALUE:nums1[partA];
-            int maxLeftB=partB==0?Integer.MIN_VALUE:nums2[partB-1];
-            int minRightB=partB==n?Integer.MAX_VALUE:nums2[partB];
+            int secondIdx=left+(right-left)/2;
+            int firstIdx=(totalLength-parity)/2-secondIdx;
+            int maxLeftA=firstIdx>0?nums1[firstIdx-1]:Integer.MIN_VALUE;
+            int minRightA=firstIdx<len1?nums1[firstIdx]:Integer.MAX_VALUE;
+            int maxLeftB=secondIdx>0?nums2[secondIdx-1]:Integer.MIN_VALUE;
+            int minRightB=secondIdx<len2?nums2[secondIdx]:Integer.MAX_VALUE;
             if(maxLeftA<=minRightB && maxLeftB<=minRightA)
-                if((m+n)%2==0)
-                    return (Math.max(maxLeftA, maxLeftB)+Math.min(minRightA, minRightB))/2.0;
+                if(parity==0)
+                    return (Math.max(maxLeftA, maxLeftB) + Math.min(minRightA, minRightB))/2.0;
                 else
-                    return Math.max(maxLeftA, maxLeftB);
+                    return (double)Math.min(minRightA, minRightB);
             else if(maxLeftA>minRightB)
-                right=partA-1;
+                left=secondIdx+1;
             else
-                left=partA+1;
+                right=secondIdx-1;
         }
         return 0.0;
     }
