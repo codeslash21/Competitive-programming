6c3a5be720bef3ab2a99bde050c0f19561b8a80c
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2025-09-29 03:17:38 +0530

    Create Main.java

---
+++ codeforces/1543A/Main.java
@@ -0,0 +1,16 @@
+// https://codeforces.com/problemset/problem/1543/A
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0) {
+            long a=sc.nextLong(), b=sc.nextLong();
+            long maxValue=Math.abs(a-b), mod=maxValue==0?0:a%maxValue, minOp=0;
+            if(Math.min(a,b)!=0 && maxValue>1)
+                minOp=Math.min(mod, maxValue-mod);
+            System.out.println(maxValue+" "+minOp);
+        }
+    }
+}
