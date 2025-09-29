eba3b8e2908d426418ebdbc6aa20ce83825604bc
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2025-09-29 11:37:12 +0530

    Create Main.java

---
+++ codeforces/1537B/Main.java
@@ -0,0 +1,13 @@
+// https://codeforces.com/problemset/problem/1537/B
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0) {
+            int n=sc.nextInt(),m=sc.nextInt(),i=sc.nextInt(),j=sc.nextInt();
+            System.out.println("1 1 "+n+" "+m);
+        }
+    }
+}
