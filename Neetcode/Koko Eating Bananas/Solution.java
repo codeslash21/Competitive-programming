14868672459c985fe4d7885b77227eaa225e6400
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2025-10-05 01:33:18 +0530

    Update Solution.java

--- Neetcode/Koko Eating Bananas/Solution.java
+++ Neetcode/Koko Eating Bananas/Solution.java
@@ -1,4 +1,4 @@
-// https://neetcode.io/problems/eating-bananas?list=neetcode150
+// https://leetcode.com/problems/koko-eating-bananas/description/
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int left=1, right=Arrays.stream(piles).max().getAsInt();
