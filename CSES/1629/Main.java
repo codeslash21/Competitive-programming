eca5987b246471ae511df40e8f139595ac49e434
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2025-10-11 21:11:47 +0530

    Update Main.java

--- CSES/1629/Main.java
+++ CSES/1629/Main.java
@@ -4,21 +4,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), count=0, currEnd=0;
-        List<List<Integer>> movieList = new ArrayList<>();
+        int n=sc.nextInt(), prev=0, cnt=0;
+        List<List<Integer>> movies = new ArrayList<>();
+        for(int i=0;i<n;i++)
+            movies.add(Arrays.asList(sc.nextInt(), sc.nextInt()));
+        Collections.sort(movies, (a,b)->a.get(1)-b.get(1));
         for(int i=0;i<n;i++) {
-            List<Integer> movie = new ArrayList<>();
-            movie.add(sc.nextInt());
-            movie.add(sc.nextInt());
-            movieList.add(movie);
-        }
-        Collections.sort(movieList, (a,b)->a.get(1)-b.get(1));
-        for(List<Integer> movie:movieList) {
-            if(movie.get(0)>=currEnd) {
-                currEnd=movie.get(1);
-                count++;
+            if(movies.get(i).get(0)>=prev) {
+                cnt++;
+                prev=movies.get(i).get(1);
             }
         }
-        System.out.println(count);
+        System.out.println(cnt);
     }
 }
