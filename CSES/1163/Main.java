77c1d17e6cf6a107f47c8de32d8018d3a0749363
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2025-10-12 16:43:47 +0530

    Update Main.java

--- CSES/1163/Main.java
+++ CSES/1163/Main.java
@@ -2,41 +2,23 @@
 import java.util.*;
 
 public class Main {
-    public static class Pair <K extends Comparable<K>, V extends Comparable<V>> implements Comparable<Pair<K,V>> {
-        K first;
-        V second;
-        public Pair(K first, V second) {
-            this.first = first;
-            this.second = second;
-        }
-        @Override
-        public int compareTo(Pair<K,V> other) {
-            int diff = this.first.compareTo(other.first);
-            if(diff==0)
-                return this.second.compareTo(other.second);
-            return diff;
-        }
-    }
-    public  static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x=sc.nextInt(), n=sc.nextInt();
-        TreeSet<Pair<Integer, Integer>> ranges = new TreeSet<>();
-        TreeMap<Integer, Integer> rangeLengths = new TreeMap<>();
-        ranges.add(new Pair(0, x));
+        TreeMap<Integer, Integer> ranges=new TreeMap<>(), rangeLengths=new TreeMap<>();
+        ranges.put(0, x);
         rangeLengths.put(x, 1);
         for(int i=0;i<n;i++) {
-            int pos = sc.nextInt();
-            Pair<Integer, Integer> range = ranges.floor(new Pair(pos, Integer.MAX_VALUE));
-            ranges.remove(range);
-            int left=range.first, right=range.second, rangeLength=right-left;
-            ranges.add(new Pair(left, pos));
-            ranges.add(new Pair(pos, right));
-            if(rangeLengths.get(rangeLength)==1)
-                rangeLengths.remove(rangeLength);
+            int pos=sc.nextInt();
+            int left=ranges.floorKey(pos), right=ranges.get(left), length=right-left;
+            ranges.put(left, pos);
+            ranges.put(pos, right);
+            if(rangeLengths.get(length)==1)
+                rangeLengths.remove(length);
             else
-                rangeLengths.put(rangeLength, rangeLengths.getOrDefault(rangeLength, 0)-1);
-            rangeLengths.put(pos-left, rangeLengths.getOrDefault(pos-left, 0)+1);
+                rangeLengths.put(length, rangeLengths.getOrDefault(length, 0)-1);
             rangeLengths.put(right-pos, rangeLengths.getOrDefault(right-pos, 0)+1);
+            rangeLengths.put(pos-left, rangeLengths.getOrDefault(pos-left, 0)+1);
             System.out.println(rangeLengths.lastKey());
         }
     }
