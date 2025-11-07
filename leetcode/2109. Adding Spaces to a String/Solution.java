ff54fa9b8d9031632bf6b7683d7f7faf89418ed7
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2025-11-07 21:48:00 +0530

    Create Solution.java

---
+++ leetcode/2109. Adding Spaces to a String/Solution.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/backspace-string-compare/
+// t.c.=O(m+n), s.c.=O(m+n)
+class Solution {
+    public String addSpaces(String s, int[] spaces) {
+        StringBuilder sb=new StringBuilder();
+        int lenS=s.length(), lenSpace=spaces.length, strIdx=0, spaceIdx=0;
+        for(;strIdx<lenS;strIdx++) {
+            if(spaceIdx< lenSpace && strIdx==spaces[spaceIdx]) {
+                sb.append(' ');
+                spaceIdx++;
+            }
+            sb.append(s.charAt(strIdx));
+        }
+        return sb.toString();
+    }
+}
