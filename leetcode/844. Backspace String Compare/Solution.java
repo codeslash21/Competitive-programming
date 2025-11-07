e2caac6d4d400ec5c2ff41d206f3c6f1e715a275
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2025-11-07 20:10:53 +0530

    Update Solution.java

--- leetcode/844. Backspace String Compare/Solution.java
+++ leetcode/844. Backspace String Compare/Solution.java
@@ -1,26 +1,25 @@
 // traversing from right
 // t.c.=O(m+n), s.c.=O(1)
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
-        int ls=s.length()-1, lt=t.length()-1;
-        int skips=0, skipt=0;
-        while(ls>=0 || lt>=0) {
-            while(ls>=0) {
-                if(s.charAt(ls)=='#')   {skips++; ls--;}
-                else if(skips>0)    {skips--; ls--;}
-                else    break;
-            }
-            while(lt>=0) {
-                if(t.charAt(lt)=='#')   {skipt++; lt--;}
-                else if(skipt>0)    {skipt--; lt--;}
-                else    break;
-            }
-            if(ls>=0 && lt>=0 && s.charAt(ls)!=t.charAt(lt))
-                return false;
-            if((ls>=0)!=(lt>=0))
-                return false;
-            ls--; lt--;
+    public int nextValidChar(String str, int idx) {
+        int backspace=0;
+        while(idx>=0) {
+            if(str.charAt(idx)=='#')    backspace++;
+            else if(backspace>0)    backspace--;
+            else    break;
+            idx--;
         }
-        return true;
+        return idx;
+    }
+    public boolean backspaceCompare(String s, String t) {
+        int lenS=s.length(), lenT=t.length(), idx1=lenS-1, idx2=lenT-1;
+        while(idx1>=0 || idx2>=0) {
+            idx1=nextValidChar(s, idx1);
+            idx2=nextValidChar(t, idx2);
+            char charS=idx1>=0?s.charAt(idx1--):' ', charT=idx2>=0?t.charAt(idx2--):' ';
+            if(charS!=charT)
+                return false;
+        }
+        return idx1<0 && idx2<0?true:false;
     }
 }
