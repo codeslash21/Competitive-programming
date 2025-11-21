9222401b85288aa0967b1e43fc731b95166859dc
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2025-11-21 16:00:25 +0530

    Update Solution.java

--- leetcode/1052. Grumpy Bookstore Owner/Solution.java
+++ leetcode/1052. Grumpy Bookstore Owner/Solution.java
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/grumpy-bookstore-owner/description/
 // Usinng sliding window
 // t.c.=O(n), s.c.=O(1)
 class Solution {
@@ -14,5 +15,22 @@ class Solution {
             satis-=(grumpy[i+1-minutes]==1?customers[i+1-minutes]:0);
         }
         return sum+maxSatis;
+    }
+}
+
+// sliding window
+// t.c.=O(n), s.c.=O(1)
+class Solution {
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int len=customers.length, satisfiedCustomers=0, unsatisfiedInWindow=0, maxUnsatisfiedInWindow=0;
+        for(int i=0;i<len;i++) {
+            satisfiedCustomers+=grumpy[i]==1?0:customers[i];
+            unsatisfiedInWindow+=grumpy[i]==1?customers[i]:0;
+            if(i>=minutes-1) {
+                unsatisfiedInWindow-=(i==minutes-1?0:grumpy[i-minutes]==1?customers[i-minutes]:0);
+                maxUnsatisfiedInWindow=Math.max(maxUnsatisfiedInWindow, unsatisfiedInWindow);
+            }
+        }
+        return satisfiedCustomers+maxUnsatisfiedInWindow;
     }
 }
