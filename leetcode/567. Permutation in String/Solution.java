fa894520b1ec3b90d08f5f53eb386901ce4c2e1d
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2025-11-23 19:19:02 +0530

    Update Solution.java

--- leetcode/567. Permutation in String/Solution.java
+++ leetcode/567. Permutation in String/Solution.java
@@ -50,33 +50,56 @@ class Solution {
     }
 }
 
-// using sliding window
-// t.c.=O(n1+n2), s.c.=O(1)
+// using frequency table
+// t.c.=O(m+(n-m)*m), s.c.=O(n-m), m=len(s1), n=len(s2)
 class Solution {
+    public boolean matchFreq(int[] arr1, int[] arr2) {
+        for(int i=0;i<26;i++)
+            if(arr1[i]!=arr2[i])
+                return false;
+        return true;
+    }
     public boolean checkInclusion(String s1, String s2) {
-        int len1=s1.length(), len2=s2.length(), seen=0;
-        int[] freq1 = new int[26], freq2 = new int[26];
-        if(len2<len1)
+        int len1=s1.length(), len2=s2.length();
+        if(len1>len2)
             return false;
+        int[] freq1=new int[26];
         for(int i=0;i<len1;i++)
             freq1[s1.charAt(i)-'a']++;
-        for(int i=0;i<len1-1;i++){
-            int c=s2.charAt(i)-'a';
-            freq2[c]++;
-            if(freq1[c]>=freq2[c])
-                seen++;
-        }
-        for(int i=len1-1;i<len2;i++) {
-            int c=s2.charAt(i)-'a';
-            freq2[c]++;
-            if(freq1[c]>=freq2[c])
-                seen++;
-            if(seen==len1)
+        for(int i=0;i<=len2-len1;i++) {
+            int[] freq2=new int[26];
+            for(int j=i;j<i+len1;j++)
+                freq2[s2.charAt(j)-'a']++;
+            if(matchFreq(freq1, freq2))
                 return true;
-            c=s2.charAt(i+1-len1)-'a';
-            if(freq1[c]>=freq2[c])
-                seen--;
-            freq2[c]--;
+        }
+        return false;
+    }
+}
+
+// using sliding window
+// t.c.=O(m+n), s.c.=O(1), m=len(s1), n=len(s2)
+class Solution {
+    public boolean matchFreq(int[] arr1, int[] arr2) {
+        for(int i=0;i<26;i++)
+            if(arr1[i]!=arr2[i])
+                return false;
+        return true;
+    }
+    public boolean checkInclusion(String s1, String s2) {
+        int len1=s1.length(), len2=s2.length();
+        if(len1>len2)
+            return false;
+        int[] freq1=new int[26], freq2=new int[26];
+        for(int i=0;i<len1;i++)
+            freq1[s1.charAt(i)-'a']++;
+        for(int i=0;i<len1-1;i++)
+            freq2[s2.charAt(i)-'a']++;
+        for(int i=len1-1;i<len2;i++) {
+            freq2[s2.charAt(i)-'a']++;
+            if(matchFreq(freq1, freq2))
+                return true;
+            freq2[s2.charAt(i-len1+1)-'a']--;
         }
         return false;
     }
