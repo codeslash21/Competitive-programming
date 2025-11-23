9ef8db4a820fc921d1d58c528b60f3adfb8b3b1e
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2025-11-23 18:31:41 +0530

    Update Solution.java

--- leetcode/567. Permutation in String/Solution.java
+++ leetcode/567. Permutation in String/Solution.java
@@ -1,3 +1,55 @@
+// https://leetcode.com/problems/permutation-in-string/description/
+// using bruteforce method, checking all possible permutations
+// t.c.=O(m!*m*n), s.c.=O(m^2), m=len(s1), n=len(s2)
+class Solution {
+    public String swap(String s, int idx1, int idx2) {
+        if(idx1==idx2)
+            return s;
+        char[] chars=s.toCharArray();
+        char temp=chars[idx1];
+        chars[idx1]=chars[idx2];
+        chars[idx2]=temp;
+        return new String(chars);
+    }
+    public boolean permute(String s1, String s2, int idx1) {
+        int len=s1.length();
+        if(idx1==len-1 && s2.indexOf(s1)>=0)
+            return true;
+        else {
+            for(int idx2=idx1;idx2<len;idx2++) {
+                s1=swap(s1, idx1, idx2);
+                if(permute(s1, s2, idx1+1))
+                    return true;
+                s1=swap(s1, idx1, idx2);
+            }
+        }
+        return false;
+    }
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length())
+            return false;
+        return permute(s1, s2, 0);
+    }
+}
+
+// using sorting
+// t.c.=O((n-m)*mlogm), s.c.=O((n-m)*(m+logm))
+class Solution {
+    public String sort(String s) {
+        char[] chars=s.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+    public boolean checkInclusion(String s1, String s2) {
+        int len1=s1.length(), len2=s2.length();
+        s1=sort(s1);
+        for(int i=0;i<=len2-len1;i++)
+            if(s1.equals(sort(s2.substring(i, i+len1))))
+                return true;
+        return false;
+    }
+}
+
 // using sliding window
 // t.c.=O(n1+n2), s.c.=O(1)
 class Solution {
