f41ac842987247f6213ce419427ec9caa492fd16
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2026-03-03 21:39:22 +0530

    Update Solution.java

--- leetcode/304. Range Sum Query 2D - Immutable/Solution.java
+++ leetcode/304. Range Sum Query 2D - Immutable/Solution.java
@@ -1,31 +1,37 @@
+// https://leetcode.com/problems/range-sum-query-2d-immutable/description/
+// using cumulative sum
+// t.c.=O(m), during initialization O(m*n), s.c.=O(m*n)
 class NumMatrix {
-    private int[] prefix;
-    private int m, n;
+    private int[][] dp;
     public NumMatrix(int[][] matrix) {
-        m=matrix.length;
-        n=matrix[0].length;
-        int idx=1;
-        prefix = new int[m*n+1];
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++){
-                prefix[idx]=prefix[idx-1]+matrix[i][j];
-                idx++;
-            }
+        int rows=matrix.length, cols=matrix[0].length;
+        dp=new int[rows][cols+1];
+        for(int row=0;row<rows;row++)
+            for(int col=0;col<cols;col++)
+                dp[row][col+1]=dp[row][col]+matrix[row][col];
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int row=row2-row1+1, col=col2-col1+1;
-        int left=row1*n+col1, sum=0;
-        for(int i=0;i<row;i++) {
-            sum+=(prefix[left+col]-prefix[left]);
-            left+=n;
-        }
+        int sum=0;
+        for(int row=row1;row<=row2;row++)
+            sum+=dp[row][col2+1]-dp[row][col1];
         return sum;
     }
 }
 
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix obj = new NumMatrix(matrix);
- * int param_1 = obj.sumRegion(row1,col1,row2,col2);
- */
+// using 2D cumulative sum
+// t.c.=O(1), O(mn) during pre-computation, s.c.=O(mn)
+class NumMatrix {
+    int[][] dp;
+    public NumMatrix(int[][] matrix) {
+        int rows=matrix.length, cols=matrix[0].length;
+        dp=new int[rows+1][cols+1];
+        for(int row=0;row<rows;row++)
+            for(int col=0;col<cols;col++)
+                dp[row+1][col+1]=dp[row+1][col]+dp[row][col+1]+matrix[row][col]-dp[row][col];
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return dp[row2+1][col2+1]-dp[row1][col2+1]-dp[row2+1][col1]+dp[row1][col1];
+    }
+}
