e884457a453e4bb9dad1f36c707238962be075de
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2026-04-05 23:42:43 +0530

    Update Solution.java

--- leetcode/3133. Minimum Array End/Solution.java
+++ leetcode/3133. Minimum Array End/Solution.java
@@ -8,3 +8,20 @@ class Solution {
         return result;
     }
 }
+
+// t.c.=O(logN), s.c.=O(1)
+class Solution {
+    public long minEnd(int n, int x) {
+        long res=x, mask=1;
+        n--;
+        while(n>0) {
+            if((x&mask)==0) {
+                if((n&1)>0)
+                    res|=mask;
+                n>>=1;
+            }
+            mask<<=1;
+        }
+        return res;
+    }
+}
