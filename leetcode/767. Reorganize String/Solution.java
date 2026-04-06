868cd821bcdd42865fa1f26f20cf2993a04bcd51
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2026-04-06 23:42:44 +0530

    Update Solution.java

--- leetcode/767. Reorganize String/Solution.java
+++ leetcode/767. Reorganize String/Solution.java
@@ -30,3 +30,35 @@ class Solution {
         return sb.toString();
     }
 }
+
+// t.c.=O(n), s.c.=O(n)
+class Solution {
+    public String reorganizeString(String s) {
+        int len=s.length(), maxAllowedFreq=(len+1)/2, maxFreq=0, maxChar=0, idx=0;
+        char[] result=new char[len];
+        int[] freq=new int[26];
+        for(int i=0;i<len;i++) 
+            freq[s.charAt(i)-'a']++;
+        for(int i=0;i<26;i++) 
+            if(freq[i]>maxFreq) {
+                maxFreq=freq[i];
+                maxChar=i;
+            }
+        if(maxFreq>maxAllowedFreq)
+            return "";
+        while(freq[maxChar]!=0) {
+            result[idx]=(char)(maxChar+'a');
+            freq[maxChar]--;
+            idx+=2;
+        }
+        for(int i=0;i<26;i++)
+            while(freq[i]!=0) {
+                if(idx>=len)
+                    idx=1;
+                result[idx]=(char)(i+'a');
+                freq[i]--;
+                idx+=2;
+            }
+        return new String(result);
+    }
+}
