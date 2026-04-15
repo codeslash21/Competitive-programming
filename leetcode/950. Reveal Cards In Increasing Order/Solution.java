c327bac971b5bd1ab3d50d51fc6ba4757066206a
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2026-04-15 11:11:27 +0530

    Create Solution.java

---
+++ leetcode/950. Reveal Cards In Increasing Order/Solution.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/reveal-cards-in-increasing-order/description/
+// using queue
+// t.c.=O(n), s.c.=O(n)
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        int len=deck.length;
+        Queue<Integer> queue=new LinkedList<>();
+        int[] res=new int[len];
+        Arrays.sort(deck);
+        for(int i=0;i<len;i++)
+            queue.add(i);
+        for(int i=0;i<len;i++) {
+            res[queue.poll()]=deck[i];
+            if(!queue.isEmpty())
+                queue.add(queue.poll());
+        }
+        return res;
+    }
+}
+
