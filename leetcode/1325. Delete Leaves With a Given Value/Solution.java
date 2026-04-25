8f327f5942a26b459fbdbddaa84a72e98257c339
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2026-04-25 19:53:51 +0530

    Create Solution.java

---
+++ leetcode/1325. Delete Leaves With a Given Value/Solution.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/delete-leaves-with-a-given-value/description/
+// t.c.=O(n), s.c.=O(n)
+class Solution {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if(root==null)  return null;
+        root.left=removeLeafNodes(root.left, target);
+        root.right=removeLeafNodes(root.right, target);
+        if(root.left==null && root.right==null && root.val==target)
+            return null;
+        return root;
+    }
+}
