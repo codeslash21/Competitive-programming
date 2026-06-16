e9c98df0bf9a29fd0599d191e1616357bfadaace
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2026-06-16 13:27:07 +0530

    Update Solution.java

--- leetcode/720. Longest Word in Dictionary/Solution.java
+++ leetcode/720. Longest Word in Dictionary/Solution.java
@@ -16,3 +16,45 @@ class Solution {
         return result;
     }
 }
+
+// using Trie
+// t.c.=O(nL), s.c.=O(nL+L), n=total words, L=max word length
+class Trie {
+    Trie[] children;
+    boolean isEnd;
+    public Trie() {
+        children=new Trie[26];
+        isEnd=false;
+    }
+    public void insert(String word) {
+        Trie node=this;
+        for(char ch:word.toCharArray()) {
+            if(node.children[ch-'a']==null)
+                node.children[ch-'a']=new Trie();
+            node=node.children[ch-'a'];
+        }
+        node.isEnd=true;
+    }
+    public String findLongestWord(Trie node, String ans, StringBuilder sb) {
+        if(sb.length()>ans.length())
+            ans=sb.toString();
+        for(int idx=0;idx<26;idx++) {
+            Trie child=node.children[idx];
+            if(child!=null && child.isEnd) {
+                sb.append((char)(idx+'a'));
+                ans=findLongestWord(child, ans, sb);
+                sb.deleteCharAt(sb.length()-1);
+            }
+        }
+        return ans;
+    }
+}
+class Solution {
+    public String longestWord(String[] words) {
+        Trie trie=new Trie();
+        trie.isEnd=true;    // as empty string also considered
+        for(String word:words)
+            trie.insert(word);
+        return trie.findLongestWord(trie, "", new StringBuilder()); 
+    }
+}
