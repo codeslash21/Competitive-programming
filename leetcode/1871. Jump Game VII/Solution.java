1062b07133518782ae62d21204253bdaba483698
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2026-06-17 21:51:22 +0530

    Update Solution.java

--- leetcode/1871. Jump Game VII/Solution.java
+++ leetcode/1871. Jump Game VII/Solution.java
@@ -20,3 +20,28 @@ class Solution {
         return false;
     }
 }
+
+// dfs with furthest pointer
+// t.c.=O(n), s.c.=O(n)
+class Solution {
+    public boolean canReach(String s, int minJump, int maxJump) {
+        int len=s.length(), furthest=0;
+        boolean[] visited=new boolean[len];
+        Queue<Integer> queue=new LinkedList<>();
+        visited[0]=true;
+        queue.offer(0);
+        while(!queue.isEmpty()) {
+            int index=queue.poll();
+            int start=Math.max(index+minJump, furthest+1);
+            int end=Math.min(index+maxJump, len-1);
+            for(int j=start;j<=end;j++) 
+                if(!visited[j] && s.charAt(j)=='0') {
+                    if(j==len-1)    return true;
+                    queue.offer(j);
+                    visited[j]=true;
+                }
+            furthest=index+maxJump;
+        }
+        return false;
+    }
+}
