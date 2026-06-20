86f0e772b3a9eea6b7b507611634327b17efcbb7
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2026-06-20 23:17:41 +0530

    Update Solution.java

--- leetcode/871. Minimum Number of Refueling Stops/Solution.java
+++ leetcode/871. Minimum Number of Refueling Stops/Solution.java
@@ -20,3 +20,41 @@ class Solution {
         }
     }
 }
+
+// using dp
+// t.c.=O(n^2), s.c.=O(n)
+class Solution {
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        int len=stations.length, minStops=Integer.MAX_VALUE;
+        int[] dp=new int[len+1];
+        dp[0]=startFuel;
+        for(int i=0;i<len;i++)
+            for(int k=i;k>=0;k--) {
+                if(dp[k]>=stations[i][0])
+                    dp[k+1]=Math.max(dp[k+1], dp[k]+stations[i][1]);
+            }
+        for(int i=0;i<=len;i++)
+            if(dp[i]>=target)
+                minStops=Math.min(minStops, i);
+        return minStops==Integer.MAX_VALUE?-1:minStops;
+    }
+}
+
+// using maxHeap
+// t.c.=O(nlogn), s.c.=O(n)
+class Solution {
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        int len=stations.length, reach=startFuel, stops=0;
+        PriorityQueue<Integer> maxHeap=new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<=len;i++) {
+            int nextPosition=i<len?stations[i][0]:target;
+            while(reach<nextPosition && !maxHeap.isEmpty()) {
+                reach+=maxHeap.poll();
+                stops++;
+            }
+            if(reach<nextPosition)  return -1;
+            if(i<len)   maxHeap.offer(stations[i][1]);
+        }
+        return stops;
+    }
+}
