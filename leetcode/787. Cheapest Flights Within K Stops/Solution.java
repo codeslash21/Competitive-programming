f9fe366d59c5db6a352a97871b197877dc8e426f
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2026-07-02 23:42:08 +0530

    Create Solution.java

---
+++ leetcode/787. Cheapest Flights Within K Stops/Solution.java
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/cheapest-flights-within-k-stops/description/
+// using Dijkstra's algo
+// 
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int INF=Integer.MAX_VALUE;
+        List<int[]>[] graph=new List[n];
+        int[][] minCost=new int[n][k+1];
+        PriorityQueue<int[]> minHeap=new PriorityQueue<>((a,b)->a[0]-b[0]);
+        // build the graph
+        for(int i=0;i<n;i++)
+            graph[i]=new ArrayList<>();
+        for(int[] flight:flights)
+            graph[flight[0]].add(new int[]{flight[1], flight[2]});
+        // initialize minCost array with max integer value
+        for(int[] row:minCost)
+            Arrays.fill(row, INF);
+        // minHeap contains pair of {cost, city, stop}
+        minHeap.offer(new int[]{0, src, -1});
+        while(!minHeap.isEmpty()) {
+            int[] curr=minHeap.poll();
+            int cost=curr[0], city=curr[1], stop=curr[2];
+            if(city==dst)   return cost;
+            if(stop==k || (stop>=0 && cost>minCost[city][stop])) continue;
+            for(int[] neighbor:graph[city]) {
+                int nextCity=neighbor[0], price=neighbor[1];
+                int nextCost=cost+price, nextStop=stop+1;
+                if(minCost[nextCity][nextStop]>nextCost) {
+                    minCost[nextCity][nextStop]=nextCost;
+                    minHeap.offer(new int[]{nextCost, nextCity, nextStop});
+                }
+            }
+        }
+        return -1;
+    }
+}
