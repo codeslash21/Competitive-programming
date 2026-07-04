b056c5f4fc49fa51695d5e9395fbccd1811078d9
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2026-07-04 11:31:17 +0530

    Update Solution.java

--- leetcode/1137. N-th Tribonacci Number/Solution.java
+++ leetcode/1137. N-th Tribonacci Number/Solution.java
@@ -4,6 +4,9 @@
 class Solution {
     public int tribonacci(int n) {
         int t0=0, t1=1, t2=1;
+        if(n==0)    return t0;
+        if(n==1)    return t1;
+        if(n==2)    return t2;
         for(int i=3;i<=n;i++) {
             int ti=t0+t1+t2;
             t0=t1;
