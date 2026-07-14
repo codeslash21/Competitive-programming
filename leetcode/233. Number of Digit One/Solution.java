99b24f7a9ec0e249098777ebab97bbf2d3a33323
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2026-07-14 15:22:34 +0530

    Create Solution.java

---
+++ leetcode/233. Number of Digit One/Solution.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/number-of-digit-one/description/
+// t.c.=O(logn), s.c.=O(1)
+class Solution {
+    public int countDigitOne(int n) {
+        int count=0;
+        for(long i=1;i<=n;i*=10) {
+            long divider=i*10;
+            count+=(n / divider) * i + Math.min(Math.max(n % divider - i + 1, 0), i);
+        }
+        return count;
+    }
+}
