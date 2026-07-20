f8c35f7d18d755a35bd20f6c2a9d4a7c30a1fe2b
Soumyadip Guha <32652085+codeslash21@users.noreply.github.com>
2026-07-20 12:05:24 +0530

    Update Solution.java

--- leetcode/307. Range Sum Query - Mutable/Solution.java
+++ leetcode/307. Range Sum Query - Mutable/Solution.java
@@ -23,3 +23,56 @@ class NumArray {
         return prefix[right+1]-prefix[left];
     }
 }
+
+// using BIT
+// t.c.=O(n) for building bit, O(logn) for update, O(logn) for query
+class FenwickTree {
+    private int len;
+    private int[] bit;
+    // Build BIT from array in O(n)
+    public FenwickTree(int[] nums) {
+        this.len=nums.length;
+        this.bit=new int[len+1];
+        // Copy values to 1-indexed BIT array
+        for(int i=0;i<len;i++)
+            bit[i+1]=nums[i];
+        for(int i=1;i<=len;i++) {
+            int parent= i + (i & (-i));
+            if(parent<=len)
+                bit[parent]+=bit[i];
+        }
+    }
+    // Add delta to position index and its parent(1-indexed)
+    public void update(int index, int delta) {
+        while(index<=len) {
+            bit[index]+=delta;
+            index=index + (index & (-index));   // go to index's parent
+        }
+    }
+    // Prefix sum from 1 to position index (1-indexed)
+    public int query(int index) {
+        int sum=0;
+        while(index>0) {
+            sum+=bit[index];
+            index=index - (index & (-index));   // go to index's child
+        }
+        return sum;
+    }
+}
+class NumArray {
+    private FenwickTree bit;
+    private int[] nums;
+    public NumArray(int[] nums) {
+        this.nums=nums;
+        this.bit=new FenwickTree(nums);
+    }
+    
+    public void update(int index, int val) {
+        bit.update(index+1, val-nums[index]);
+        nums[index]=val;
+    }
+    
+    public int sumRange(int left, int right) {
+        return bit.query(right+1) - bit.query(left);
+    }
+}
